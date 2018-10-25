dfecbe77a6e535b9d9f5f0b41b1e4bca46272dd9
Hoodrn03 <Hoodrn03@gmail.com>
2018-10-25 10:29:54 +0100

    Added additional input commands.
    I have added a few extra input commands into the program allowing for green and blue manipulation with extra keys.

--- Input/include/InputHandler.h
+++ Input/include/InputHandler.h
@@ -24,6 +24,30 @@ public:
 	void execute(GameObject& playerBackground) override { playerBackground.r -= COLOUR_ADJUSTMENT_VALUE; }
 };
 
+class IncreaseGreenCommand : public InputCommand
+{
+public:
+	void execute(GameObject& playerBackground) override { playerBackground.g += COLOUR_ADJUSTMENT_VALUE; }
+};
+
+class DecreaseGreenCommand : public InputCommand
+{
+public:
+	void execute(GameObject& playerBackground) override { playerBackground.g -= COLOUR_ADJUSTMENT_VALUE; }
+};
+
+class IncreaseBlueCommand : public InputCommand
+{
+public:
+	void execute(GameObject& playerBackground) override { playerBackground.b += COLOUR_ADJUSTMENT_VALUE; }
+};
+
+class DecreaseBlueCommand : public InputCommand
+{
+public:
+	void execute(GameObject& playerBackground) override { playerBackground.b -= COLOUR_ADJUSTMENT_VALUE; }
+};
+
 struct InputHandler
 {	
 	GameObject* m_playerBackground;
@@ -37,6 +61,12 @@ struct InputHandler
 		// and changed on the fly
 		m_controlMapping[65] = new IncreaseRedCommand;
 		m_controlMapping[90] = new DecreaseRedCommand;
+
+		m_controlMapping[83] = new IncreaseGreenCommand;
+		m_controlMapping[88] = new DecreaseGreenCommand; 
+
+		m_controlMapping[68] = new IncreaseBlueCommand;
+		m_controlMapping[67] = new DecreaseBlueCommand; 
 	}
 
 	void handleInputs(const std::vector<bool>& keyBuffer)
