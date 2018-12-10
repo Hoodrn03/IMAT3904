97a549ec105db383c034df60f5ba06113adbe6ae
Hoodrn03 <Hoodrn03@gmail.com>
2018-12-10 11:58:37 +0000

    Begun work on key commands
    I have created a class which will act as the base for the initial key command. It will act as a baseline which I will follow when working on the other commands.

--- Lab5/include/InputHandler.h
+++ Lab5/include/InputHandler.h
@@ -15,6 +15,11 @@ public:
 
 //e.g. class RotateLeftCommand : public InputCommand
 
+class RotateLeftCommand : public InputCommand
+{
+	
+};
+
 
 struct InputHandler
 {	
@@ -26,7 +31,7 @@ struct InputHandler
 	{
 		// the idea will be to map the keys directly from a config file that can be loaded in
 		// and changed on the fly
-		//m_controlMapping[65] = 
+		m_controlMapping[65] = nullptr;
 		//m_controlMapping[90] = 
 		//m_controlMapping[83] = 
 		//m_controlMapping[88] = 
