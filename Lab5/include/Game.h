b999b4132993fb9ce6f3d1066b9a78627bf573cb
Hoodrn03 <Hoodrn03@gmail.com>
2018-12-10 11:43:50 +0000

    Comments
    I have moved a few things around and done some simple comments.

--- Lab5/include/Game.h
+++ Lab5/include/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "IEngineCore.h"
 #include "InputHandler.h"
 #include "GameObject.h"
@@ -6,20 +7,56 @@
 
 class Game
 {
+
 public:
+
+	//-----------------------------------------------------------//
+	/*! Constructor 
+	*
+	*/
 	Game();
+	
+	//-----------------------------------------------------------//
+	/*! Deconstructor
+	*
+	*/
+	~Game();
+
+	// Data Members 
+
+private:
+
+	/*! \var The background for the game. */
+	GameObject m_playerBackground;
+
+	/*! \var The initial Cube dissplayed within the game world. */
+	GameObject m_playerCube;
+
+	/*! \var This will be the main game camera for the game. */
+	Camera m_camera;
+
+public:
+
 	IEngineCore* m_engineInterfacePtr;
 
 	InputHandler* m_inputHandler;
 
+	// Member Functons 
+
+public:
+
+	//-----------------------------------------------------------//
+	/*! Update : Called once every frame. 
+	*
+	*/
 	void update();
+
+	//-----------------------------------------------------------//
+	/*! Render : Will draw all items at the end of the frame. 
+	*
+	*/
 	void render();
 
 	void loadLevel(std::string levelFile);
 
-private:
-	GameObject m_playerBackground;
-	GameObject m_playerCube;
-
-	Camera m_camera;
 };
--- Lab5/src/Game.cpp
+++ Lab5/src/Game.cpp
@@ -4,6 +4,10 @@
 #include <fstream>
 #include <sstream>
 
+//-----------------------------------------------------------//
+/*! Constructor
+*
+*/
 Game::Game()
 {
 	m_engineInterfacePtr = nullptr;
@@ -20,21 +24,45 @@ Game::Game()
 	temp->translate(0, 0, -5);
 }
 
+//-----------------------------------------------------------//
+/*! Deconstructor
+*
+*/
+Game::~Game()
+{
+}
+
+//-----------------------------------------------------------//
+/*! Update : Called once every frame.
+*
+*/
 void Game::update()
 {
 	
 }
 
+//-----------------------------------------------------------//
+/*! Render : Will draw all items at the end of the frame.
+*
+*/
 void Game::render()
 {
 	float redValue = 0, greenValue = 0, blueValue = 0;
-	if(m_playerBackground.getComponent<RedComponent>())
+
+	if (m_playerBackground.getComponent<RedComponent>())
+	{
 		redValue = m_playerBackground.getComponent<RedComponent>()->m_colourValue;
-	if (m_playerBackground.getComponent<GreenComponent>())
-		greenValue = m_playerBackground.getComponent<GreenComponent>()->m_colourValue;
-	if (m_playerBackground.getComponent<BlueComponent>())
-		blueValue = m_playerBackground.getComponent<BlueComponent>()->m_colourValue;
+	}
 	
+	if (m_playerBackground.getComponent<GreenComponent>())
+	{
+		greenValue = m_playerBackground.getComponent<GreenComponent>()->m_colourValue;
+	}
+	if (m_playerBackground.getComponent<BlueComponent>())
+	{
+		blueValue = m_playerBackground.getComponent<BlueComponent>()->m_colourValue;
+	}
+
 	// e.g. pass object details to the engine to render the next frame
 	m_engineInterfacePtr->renderColouredBackground(redValue, greenValue, blueValue);
 
